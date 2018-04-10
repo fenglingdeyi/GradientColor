5baad4857cb7d83d8bcfed89f54e4d00930641cb
hh <972958298@qq.com>
2018-04-10 17:53:22 +0800

    0.0.2

--- GradientColor/AppDelegate.m
+++ GradientColor/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,7 +18,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+    
+    ViewController *vvc = [[ViewController alloc] init];
+    self.window.rootViewController = vvc;
+    
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
--- GradientColor/ViewController.m
+++ GradientColor/ViewController.m
@@ -6,7 +6,14 @@
 //  Copyright © 2018年 Yunyu. All rights reserved.
 //
 
+//那我们就让美工给出的 参数/255 得到一个0-1之间的数
+#define RGBA(r,g,b,a) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:a]
+//传入RGB三个参数，得到颜色
+#define RGB(r,g,b) RGBA(r,g,b,1.f)
+
 #import "ViewController.h"
+
+#import "UIImage+GradientColor.h"
 
 @interface ViewController ()
 
@@ -17,7 +24,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self createView];
+    
 }
+
+-(void)createView{
+    
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(20, 100, 200, 500)];
+    [imageView setImage:[UIImage gradientColorImageFromColors:@[RGB(100, 0, 0),RGB(0, 100, 50)] gradientType:GradientTypeTopToBottom imgSize:imageView.frame.size]];
+    [self.view addSubview:imageView];
+    
+    
+}
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
