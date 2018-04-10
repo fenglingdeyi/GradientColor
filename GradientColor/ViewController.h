d110d39064bcbcc62b8f2a6ceba57978e363c95e
hh <972958298@qq.com>
2018-04-10 15:06:24 +0800

    第一次

---
+++ GradientColor/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GradientColor
+//
+//  Created by dongkaiqiang on 2018/4/10.
+//  Copyright © 2018年 Yunyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
